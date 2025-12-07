265c73f79b829ee856ebf3a79ed291935b3f0bba
5kappa <skappa.lmao@gmail.com>
2025-12-07 14:56:31 +0800

    Finalized Ssd and Motherboard models

--- src/com/parts/model/Ssd.java
+++ src/com/parts/model/Ssd.java
@@ -1,7 +1,37 @@
 package com.parts.model;
 
 public class Ssd extends Component {
+    // Properties for compatibility checks
+    private String interfaceType;               // ! NOT IN DATASET
+
+    // SSD-specific properties
     private String formFactor;
+    private String protocol;
     private int capacityGBs;
+    private boolean isNVMe;
+
+    public Ssd(String id, String name, String manufacturer, double price,
+               String interfaceType, String formFactor, String protocol,
+               int capacityGBs, boolean isNVMe) {
     
+        super(id, name, manufacturer, price);
+
+        this.interfaceType = interfaceType;
+
+        this.formFactor = formFactor;
+        this.protocol = protocol;
+        this.capacityGBs = capacityGBs;
+        this.isNVMe = isNVMe;
+    }
+
+    public String getInterfaceType() { return interfaceType; }
+    public String getFormFactor() { return formFactor; }
+    public String getProtocol() { return protocol; }
+    public int getCapacityGBs() { return capacityGBs; }
+    public boolean isNVMe() { return isNVMe; }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d GB", super.toString(), capacityGBs);
+    }
 }
