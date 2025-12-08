fd34acbf695c2ae32e9c53bad7fad6d0b1f9f664
5kappa <skappa.lmao@gmail.com>
2025-12-08 23:54:32 +0800

    Added interfaceType to Hdd model

--- src/com/parts/model/Hdd.java
+++ src/com/parts/model/Hdd.java
@@ -3,6 +3,7 @@ package com.parts.model;
 public class Hdd extends Component {
     // Properties for compatibility checking
     private String formFactorInches;
+    private String interfaceType;
 
     // HDD-specific properties
     private int sizeTB;
@@ -10,17 +11,22 @@ public class Hdd extends Component {
     private int cache;
 
     public Hdd(String id, String name, String manufacturer, double price,
-               String formFactorInches, int sizeTB, int RPM, int cache) {
+               String formFactorInches, String interfaceType, int sizeTB,
+               int RPM, int cache) {
         
         super(id, name, manufacturer, price);
 
         this.formFactorInches = formFactorInches;
+        this.interfaceType = interfaceType;
+
         this.sizeTB = sizeTB;
         this.RPM = RPM;
         this.cache = cache;
     }
 
     public String getFormFactorInches() { return formFactorInches; }
+    public String getInterfaceType() { return interfaceType; }
+
     public int getSizeTB() { return sizeTB; }
     public int getRPM() { return RPM; }
     public int getCache() { return cache; }
