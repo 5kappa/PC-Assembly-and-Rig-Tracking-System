6c06a7a09e0a175d74623d7d5bb5edd8f2171984
5kappa <skappa.lmao@gmail.com>
2025-12-19 18:32:40 +0800

    Added USB header fields

--- src/com/parts/model/Motherboard.java
+++ src/com/parts/model/Motherboard.java
@@ -1,5 +1,7 @@
 package com.parts.model;
 
+// Missing fanHeaders; altho if a user runs out of headers they just buy a splitter/hub
+
 public class Motherboard extends Component {
     // Properties for compatibility checking
     private String socket;
@@ -11,6 +13,10 @@ public class Motherboard extends Component {
     private int pcieX1SlotCount;
     private int pcieX4SlotCount;
     private int pcieX8SlotCount;
+    private int usb20HeaderCount;
+    private int usb32Gen1HeaderCount;
+    private int usb32Gen2HeaderCount;
+    private int usb32Gen2x2HeaderCount;
 
     // Motherboard-specific properties
     private String chipset;
@@ -19,13 +25,14 @@ public class Motherboard extends Component {
 
     // Storage ports
     private int sataPortCount;
-    private int m2SlotCount;            // ! NOT IN DATASET
+    private int m2SlotCount;
     
     public Motherboard(String id, String name, String manufacturer, double price, String socket,
                        String ramType, int ramSlotCount, int maxRamCapacityGBs, String formFactor,
                        int pcieX16SlotCount, int pcieX1SlotCount, int pcieX4SlotCount,
-                       int pcieX8SlotCount, String chipset, boolean hasWifi, String primaryColor,
-                       int sataPortCount, int m2SlotCount) {
+                       int pcieX8SlotCount, int usb20HeaderCount, int usb32Gen1HeaderCount,
+                       int usb32Gen2HeaderCount, int usb32Gen2x2HeaderCount, String chipset,
+                       boolean hasWifi, String primaryColor, int sataPortCount, int m2SlotCount) {
 
         super(id, name, manufacturer, price);
 
@@ -38,6 +45,10 @@ public class Motherboard extends Component {
         this.pcieX1SlotCount = pcieX1SlotCount;
         this.pcieX4SlotCount = pcieX4SlotCount;
         this.pcieX8SlotCount = pcieX8SlotCount;
+        this.usb20HeaderCount = usb20HeaderCount;
+        this.usb32Gen1HeaderCount = usb32Gen1HeaderCount;
+        this.usb32Gen2HeaderCount = usb32Gen2HeaderCount;
+        this.usb32Gen2x2HeaderCount = usb32Gen2x2HeaderCount;
 
         this.chipset = chipset;
         this.hasWifi = hasWifi;
@@ -56,6 +67,10 @@ public class Motherboard extends Component {
     public int getPcieX1SlotCount() { return pcieX1SlotCount; }
     public int getPcieX4SlotCount() { return pcieX4SlotCount; }
     public int getPcieX8SlotCount() { return pcieX8SlotCount; }
+    public int getUsb20HeaderCount() { return usb20HeaderCount; }
+    public int getUsb32Gen1HeaderCount() { return usb32Gen1HeaderCount; }
+    public int getUsb32Gen2HeaderCount() { return usb32Gen2HeaderCount; }
+    public int getUsb32Gen2x2HeaderCount() { return usb32Gen2x2HeaderCount; }
 
     public String getChipset() { return chipset; }
     public boolean hasWifi() { return hasWifi; }
